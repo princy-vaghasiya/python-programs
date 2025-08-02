7c269e5ab6da4bdb6b3571466c2a5ab7da07bd25
princy-vaghasiya <bvaghasiya069@gmail.com>
2025-08-02 14:09:58 +0530

    heap sort.py

--- heap sort.py
+++ heap sort.py
@@ -1,25 +1,28 @@
-def heapify(arr,n,i):
-    while True:
-        largest=i
-        left=2*i+1
-        right=2*i+2
-        if left<n and arr[left]>arr[largest]:
-            largest=left
-        if right<n and arr[right]>arr[largest]:
-            largest=right
-        if largest==i:
-            break
-        arr[i],arr[largest]=arr[largest],arr[i]
-        i=largest
+def heapify(arr, n, i):
+    largest = i
+    left = 2 * i + 1
+    right = 2 * i + 2
+
+    if left < n and arr[left] > arr[largest]:
+        largest = left
+
+    if right < n and arr[right] > arr[largest]:
+        largest = right
+
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(arr, n, largest)
+        
 def heap_sort(arr):
     n = len(arr)
-    for i in range(n // 2 ):
+    for i in reversed(range(n // 2)):
         heapify(arr, n, i)
-    for i in range(n-1,0,-1):
-        arr[0],arr[i]=arr[i],arr[0]
-        heapify(arr,i,0)
 
-arr=[3,4,5,1,2,8]
-print("original array:",arr)
+    for i in range(n - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i] 
+        heapify(arr, i, 0)
+
+arr = [3,4,1,6,8,5]
+print("Original array:", arr)
 heap_sort(arr)
-print("sorted array(ascending):",arr)
+print("Sorted array(ascending):", arr)
